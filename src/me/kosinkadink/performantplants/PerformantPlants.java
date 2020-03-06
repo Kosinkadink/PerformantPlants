5d4c1d47826ef96ca8ab455ef1a95db4bd743caa
Jedrzej Kosinski <kosinkadink1@gmail.com>
2020-03-05 19:35:26 -0600

    first commit

---
+++ src/me/kosinkadink/performantplants/PerformantPlants.java
@@ -0,0 +1,16 @@
+package me.kosinkadink.performantplants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PerformantPlants extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
